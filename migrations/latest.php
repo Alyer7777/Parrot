f1347da8984b725649109068808acd8b61de4967
Alyer77777 <romeotamere@gmail.com>
2023-09-20 14:16:53 +0200

    -A

--- migrations/latest.php
+++ migrations/latest.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230920101823 extends AbstractMigration
+final class latest extends AbstractMigration
 {
     public function getDescription(): string
     {
