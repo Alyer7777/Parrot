077fae403a539114cea248884931041be364c9b4
Alyer77777 <romeotamere@gmail.com>
2023-09-20 18:40:32 +0200

    -A

--- public/scripts/vehicules.js
+++ public/scripts/vehicules.js
@@ -1,6 +1,15 @@
-let slider = document.querySelectorAll("mySlides1", "mySlides2", "mySlides3", "mySlides4" , "mySlides5");
-let value = document.querySelectorAll("value");
-value.textContent = slider.value;
-slider.oninput = function(){
-    value.textContent = this.value;
-};
+const slider1 = document.getElementById("slider1");
+const slider2 = document.getElementById("slider2");
+
+slider1.addEventListener("input", handleSliderChange);
+slider2.addEventListener("input", handleSliderChange);
+
+function handleSliderChange () {
+    const value1 = slider1.value;
+    const value2 = slider2.value;
+
+    console.log("Slider 1 value:", value1);
+    console.log("Slider 2 value:", value2);
+
+}
+
