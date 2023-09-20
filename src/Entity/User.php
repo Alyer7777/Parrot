1cf842382410ac489fb1610c51f0e49611dd5bcb
Alyer77777 <romeotamere@gmail.com>
2023-09-20 16:56:09 +0200

    -A

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,24 +2,24 @@
 
 namespace App\Entity;
 
-use App\Repository\MainRepository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity(repositoryClass: MainRepository::class)]
-class Main
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 100)]
     private ?string $name = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 100)]
     private ?string $surname = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 100)]
     private ?string $password = null;
 
     public function getId(): ?int
