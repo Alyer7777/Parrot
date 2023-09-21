9e584cd1d3747a8c336a83cfcd4a2febc050a87c
Alyer77777 <romeotamere@gmail.com>
2023-09-21 15:16:17 +0200

    -A

--- migrations/Version20230921131348.php
+++ migrations/Version20230921131348.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230921124104 extends AbstractMigration
+final class Version20230921131348 extends AbstractMigration
 {
     public function getDescription(): string
     {
